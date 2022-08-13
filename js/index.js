1f2c9f306e2531e14b10f2a1b2650d8446e69a02
+ <+>
2022-08-13 12:22:52 +0300

    Done

--- js/index.js
+++ js/index.js
@@ -3,19 +3,21 @@
   greetProgrammer();
   => "Hello, programmer!"
 */
-function greetProgrammer() {
-  console.log("Hello, programmer!");
-}
+// function greetProgrammer() {
+//   console.log("Hello, programmer!");
+// }
+// greetProgrammer()
 
 /*
   You should be able to call this function with one argument and see its output in the terminal:
   greet("Naureen");
   => "Hello, Naureen!"
 */
-function greet(name) {
-  console.log(`Hello, ${name}!`);
-}
-
+// function greet(name) {
+//   console.log(`Hello, ${name}!`);
+// }
+// greet()
+// greet ("Hello, Naureen!")
 /*
   You should be able to call this function with no arguments and see its output in the terminal:
   greetWithDefault();
@@ -25,19 +27,21 @@ function greet(name) {
   greetWithDefault("Sunny");
   => "Hello, Sunny!"
 */
-function greetWithDefault(name = "programmer") {
-  console.log(`Hello, ${name}!`);
-}
-
+// function greetWithDefault(name = "programmer") {
+//   console.log(`Hello, ${name}!`);
+// }
+// greetWithDefault
+// greetWithDefault 
 /*
   You should be able to call this function with two arguments and get back its return value:
   const sum = add(1, 2);
   console.log(sum);
   => 3
 */
-function add(num1, num2) {
-  return num1 + num2;
-}
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
 
 /*
   You should be able to call this function with two arguments and get back its return value:
@@ -49,8 +53,8 @@ function add(num1, num2) {
   const result = halve("two")
   => null
 */
-function halve(number) {
-  if (typeof number !== "number") return null;
+// function halve(number) {
+//   if (typeof number !== "number") return null;
 
-  return number / 2;
-}
+//   return number / 2;
+// }
--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+greet_programmer()
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet "Hello, Naureen!"
+greet "Hello, Jimmy!"
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+greet_with_default "Naureen"
+
+
+def add(num1 ,num2)
+   num1 + num2
+end
+
+
+
+def halve(num)
+    return nil unless num.class == Integer
+  
+    num / 2
+end
+
+
+
+
